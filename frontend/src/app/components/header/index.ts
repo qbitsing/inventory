be888da334f3538d9301c720c36ed466ba56ec24
Carlos Ardila <cfardila9@misena.edu.co>
2017-03-02 20:04:42 -0500

    Web service y login component

--- frontend/src/app/components/header/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the HeaderComponent.
- */
-export * from './header.component';
---
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * Test Component Login
+ */
+import {LoginComponent} from "./login.component";
+import {Component} from "@angular/core";
+@Component({
+    selector: 'test-login',
+    template: '<sd-login></sd-login>',
+    directives: []
+})
+class TestLoginComponent {}
