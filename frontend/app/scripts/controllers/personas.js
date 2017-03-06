71c0e9498662ceb3ef84c62e962e9f1035c0fa18
Carlos Ardila <cfardila9@misena.edu.co>
2017-03-06 11:58:45 -0500

    Cambios

--- frontend/app/scripts/controllers/personas.js
+++ frontend/app/scripts/controllers/personas.js
@@ -8,9 +8,9 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('PersonasCtrl', function ($scope, $timeout, $state, SesionUsuario) {
-  	if(SesionUsuario.obtenerSesion()==null){
-      $state.go('Login');
+.controller('PersonasCtrl', function ($scope, $timeout, $state, SesionUsuario, webServer) {
+    if(SesionUsuario.obtenerSesion()==null){
+        $state.go('Login');
     }
     $scope.panelAnimate='';
     $scope.pageAnimate='';  
@@ -23,6 +23,19 @@ angular.module('frontendApp')
     $scope.Persona=[];
     $scope.Persona.rol={};
     $scope.EnviarPersona=function(){
-      console.log($scope.Persona);
+        var ruta="";
+        var metodo="";
+        if ($scope.panel_title_form=="Registro de Personas") {
+            ruta="personas";
+            metodo="post";
+        }else{
+            ruta="personas/"+$scope.Persona.documento;
+            metodo="put";
+        }
+        webServer.getResource(ruta,$scope.Persona,metodo).then(function(data){
+           console.log(data);
+        },function(data){
+          alert(data.data.message);
+        });
     }
-  });
+});
