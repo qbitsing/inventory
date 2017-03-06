933e38d5e3a618a65e647243b00083fa348460ff
omar plata <oommaarr@misena.edu.co>
2017-03-06 17:29:39 -0500

    arreglo en el listar personas

---
+++ backend/app/controllers/materia-prima.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const materiaPrimaModel = require('../materia-prima');
+
+function listarAll(req, res){
+
+}
+
+function listarById(req, res){
+
+}
+
+function crear(req, res){
+
+}
+
+function actualizar(req, res){
+
+}
+
+function eliminar(req, res){
+    
+}
--- backend/app/models/materia-prima.js
+++ backend/app/models/materia-prima.js
@@ -2,12 +2,10 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const unidadModel = require('./unidades');
-console.log(unidadModel);
 
 const materia_primaSchema = Schema({
     nombre: String,
-    unidad_medida: unidadModel,
+    unidad_medida: {},
     min_stock: Number,
     cantidad: Number
 });
