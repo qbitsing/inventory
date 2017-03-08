2b588f1500c17a26ade373eaa10fd5558d946f13
Carlos Ardila <cfardila9@misena.edu.co>
2017-03-08 13:25:47 -0500

    Cambios

--- frontend/app/scripts/controllers/perfil.js
+++ frontend/app/scripts/controllers/perfil.js
@@ -20,6 +20,9 @@ angular.module('frontendApp')
     },100);
     $scope.panel_title_form = "Perfil";
     $scope.button_title_form = "Actualizar datos";
+    $scope.Usuario=SesionUsuario.obtenerSesion();
+    $scope.MiUsuario={};
+    $scope.cambio=false;
     var handleFileSelect=function(evt) {
         angular.element(document.querySelector('#inputval')).text( $(this).val());
         var file=evt.currentTarget.files[0];
@@ -27,10 +30,40 @@ angular.module('frontendApp')
         reader.onload = function (evt) {
           $scope.$apply(function($scope){
             $scope.contador=4;
-            $scope.cambiar();
             $scope.myImage=evt.target.result;
           });
         };
-          reader.readAsDataURL(file);
-      };
+        reader.readAsDataURL(file);
+    };
+    $scope.cambiar=function(act){
+        if(act==1){
+            $scope.contador=1;
+        }else if(act==2){
+            $scope.contador++;
+        }
+        if($scope.contador>=3){
+            $scope.cambio=true;
+        }
+    }
+    $scope.ImageUpdate=function(){
+        if($scope.cambio){
+            $scope.MiUsuario.myImage=$scope.myCroppedImage;
+            $scope.MiUsuario.Image=$scope.myImage;
+            webServer
+            .getResource('Usuarios/ActualizarImagen/' , $scope.MiUsuario,'put')
+            .then(function(data){
+                EnviarDatos(1);
+            },function(data){
+            });
+        }else{
+            EnviarDatos(0);
+        }
+    }
+    $scope.EnviarDatos=function(actualizo){
+        if(JSON.stringify($scope.Usuario)==JSON.stringify(SesionUsuario.obtenerSesion())){
+            console.log($scope.Usuario);
+        }else{
+            console.log('Cambio');
+        }
+    }
   });
