138592801a477ea8f382a1eebfb9e9840e43c385
omar plata <oommaarr@misena.edu.co>
2017-03-14 10:24:08 -0500

    Esquema orden venta

---
+++ backend/app/models/orden-venta.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ordenVentaSchema = new Schema({
+    cliente: {},
+    consecutivo: {type: Number, index: true},
+    orden_compra_cliente: String,
+    fecha_recepcion: Date,
+    lugar_entrega: String,
+    productos: [],
+    obcervaciones: String,
+    fecha_entrega: Date,
+    estado: {type: String, enum:['Activo' , 'Inactivo'], default: 'Activo'}
+});
