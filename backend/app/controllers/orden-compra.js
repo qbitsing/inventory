7c1e4ad9ef01ec18f2f6a13b3069a89d910fed2f
OmarAndres7 <oommaarr@misena.edu.co>
2017-05-03 09:43:22 -0500

    Cambios

--- backend/app/controllers/orden-compra.js
+++ backend/app/controllers/orden-compra.js
@@ -4,35 +4,34 @@ const ordenCompraModel = require('../models/orden-compra');
 const proveedorModel = require('../models/personas');
 const productoModel = require('../models/productos');
 const materiaPrimaModel = require('../models/materia-prima');
+const co = require('co');
 
-function listarAll(req, res){
-    ordenCompraModel.find({},null, {sort: {fecha: -1}} , (err , ordenStored)=>{
-        if(err){
-            return res.status(500).send({
-                message: `ERROR al intentar obtener la lista de ordenes de compra ${err}`
-            });
-        }
 
-        if(ordenStored.length < 1){
+let listarAll = co.wrap(function * (req, res){
+    try {
+        let datos = yield ordenCompraModel.find({}, null, {sort: {fecha: -1}});
+
+        if(datos.length < 1) {
             return res.status(404).send({
                 message: `ERROR no hay ordenes de compra registradas`
             });
         }
 
         return res.status(200).send({
-            datos : ordenStored
+            datos
         });
-    });
-}
+    } catch (e) {
+        return res.status(500).send({
+            message:`ERROR ${e}`
+        });
+    }
+});
 
-function listarById(req, res){
-    let ordenId = req.params.id;
-    ordenCompraModel.findById(ordenId, (err , ordenStored)=>{
-        if(err){
-            return res.status(500).send({
-                message : `ERROR al intentar obtener el recurso ${err}`
-            });
-        }
+let listarById = co.wrap(function * (req, res){
+    try {
+        let ordenId = req.params.id;
+
+        let datos = yield ordenCompraModel.findById(ordenId);
 
         if(!productoStrored){
             return res.status(404).send({
@@ -43,48 +42,45 @@ function listarById(req, res){
         return res.status(200).send({
             datos : ordenStored
         });
-
-    });
-}
-
-function crear(req, res){
-    pasoUno();
-    function pasoUno(){
-        if(req.body.proveedor){
-            proveedorModel.findById(req.body.proveedor._id , (err, proveedorStrored)=>{
-                if(err){
-                    return res.status(500).send({
-                        message : `ERROR al obtener el proveedor ${err}`
-                    });
-                }
-
-                if(!proveedorStrored){
-                    return res.status(404).send({
-                        message : `ERROR el proveedor indicado no s encuentra en la base de datos`
-                    });
-                }
-
-                req.body.proveedor = proveedorStrored;
-                pasoDos();
-            });
-        }else pasoDos();
-    }
-
-    function pasoDos(){
-        let newOrdenCompra = new ordenCompraModel(req.body);
-        newOrdenCompra.save((err , ordenStored)=>{
-            if(err){
-                return res.status(500).send({
-                    message : `ERROR al intentar almacenar el recurso en la base de datos ${err}`
-                });
-            }
-
-            return res.status(200).send({
-                datos: ordenStored
-            });
+    } catch (e) {
+        return res.status(500).send({
+            message : `ERROR ${e}`
         });
     }
-}
+});
+
+let crear = co.wrap(function * (req, res){
+    try {
+        let proveedor;
+        if(req.body.proveedor){
+            let proveedor = yield proveedorModel.findById(req.body.proveedor._id);
+
+            if(!proveedor){
+                return res.status(404).send({
+                    message: `ERROR el proveedor indicado no s encuentra en la base de datos`
+                });
+            }
+        }
+
+        req.body.proveedor = proveedor;
+
+        let newOrdenCompra = new ordenCompraModel(req.body);
+
+        let datos = yield newOrdenCompra.save();
+
+        return res.status(200).send({
+            message: 'Orden de Compra registrada con exito',
+            id: datos._id,
+            datos
+        });
+
+
+    } catch (e) {
+        return res.status(500).send({
+            message: `ERROR ${e}`
+        });
+    }
+});
 
 function actualizar(req, res){
     pasoUno();
