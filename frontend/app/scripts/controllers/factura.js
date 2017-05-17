0cdcfbdf15f782bef0a791dba6e39675d1627317
OmarAndres7 <oommaarr@misena.edu.co>
2017-05-17 16:37:32 -0500

    Cambios

--- frontend/app/scripts/controllers/factura.js
+++ frontend/app/scripts/controllers/factura.js
@@ -8,13 +8,74 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-.controller('FacturaCtrl', function ($scope, $state, preloader, server, webServer, numeroaletras) {
+.controller('FacturaCtrl', function ($scope, $state, preloader,BotonesTabla, server, webServer, numeroaletras, $timeout) {
+    $scope.panelAnimate='';
+	$scope.pageAnimate='';
+    $(document).ready(function(){
+        $('.modal').modal();
+        $('.modal').modal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: 0, // Opacity of modal background
+            inDuration: 300, // Transition in duration
+            outDuration: 200, // Transition out duration
+            startingTop: '10%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute,
+            ready: function(modal, trigger) {
+            },
+            complete: function() {  } // Callback for Modal close
+        });
+    });
+	$timeout(function () {
+        $scope.pageAnimate='pageAnimate';
+        $scope.panelAnimate='panelAnimate';
+    },100);
+    $scope.newFactura = true;
     $scope.server = server;
     $scope.fecha = fechaHoy();
     $scope.iva = 19;
     $scope.valorIva = 0;
     $scope.subtotal = 0;
     $scope.total = 0;
+    $scope.Facturas = [];
+    $scope.Detalle = {};
+    var casillaDeBotones = '<div>'+BotonesTabla.Detalles+BotonesTabla.Borrar+BotonesTabla.Imprimir+'</div>';
+    $scope.gridOptions = {
+        columnDefs: [
+            {
+                name:'no. orden',field: 'consecutivo',
+                width:'10%',
+                minWidth: 100
+            },
+            {
+                name:'fecha',
+                width:'15%',
+                cellTemplate: '<div>{{grid.appScope.convertirFecha(row.entity.fecha)}}</div>',
+                minWidth: 100
+            },
+            {
+                field:  'subtotal',
+                width:'15%',
+                minWidth: 100
+            },
+            { 
+                name: 'Iva',
+                field: 'valorIva',
+                width:'15%',
+                minWidth: 100
+            },
+            { 
+                field: 'total',
+                width:'15%',
+                minWidth: 100
+            },
+            {
+                name: 'Opciones', enableFiltering: false, cellTemplate :casillaDeBotones,
+                width:'30%',
+                minWidth: 230
+            }
+        ]
+    }
+
     $scope.print = function(){
         preloader.estado = true;
         var factura = {
@@ -28,7 +89,7 @@ angular.module('frontendApp')
             vencimiento: $scope.vencimiento,
             remision: $scope.remision,
             ordenCompra: $scope.ordenCompra,
-            orden: 'orden '+$scope.Orden._id,
+            orden: $scope.Orden._id,
             consecutivo: $scope.Orden.orden_venta_consecutivo
         };
         if(!$scope.remision){
@@ -57,15 +118,26 @@ angular.module('frontendApp')
         });
                
     }
-
     webServer.getResource('orden_venta/'+$state.params._id, {} , 'get')
     .then(function(data){
         $scope.Orden = data.data.datos;
         $scope.calcularTotal();
     }, function(data){
-        sweetAlert('Oops...', data.data.message, 'error');
+        $scope.newFactura = false;
+        $scope.listarFacturas();
+        
     });
 
+    $scope.listarFacturas = function(){
+        webServer.getResource('facturas', {} , 'get')
+        .then(function(data){
+            $scope.Facturas = data.data.datos;
+            $scope.gridOptions.data = $scope.Facturas;
+        }, function(data){
+            $scope.gridOptions.data = $scope.Facturas;
+            $state.go('OrdenVenta');
+        });
+    }
     $scope.calcularTotal= function(){
         $scope.subtotal = 0;
         $scope.Orden.productos.forEach(function(pro){
@@ -73,7 +145,6 @@ angular.module('frontendApp')
         });
         $scope.calcularIva();
     }
-
     $scope.calcularIva = function (){
         $scope.valorIva = $scope.subtotal * ($scope.iva / 100);
         $scope.total = $scope.subtotal + $scope.valorIva;
@@ -84,11 +155,104 @@ angular.module('frontendApp')
         date += ' / '+new Date().getFullYear();
         return date;
     }
-
-   
-
     $scope.covert = function (argument) {
         return numeroaletras.doit(argument);
     }
+
+    $scope.convertirFecha = function(fecha){
+        var date = new Date(fecha).getDate();
+        date += '/'+(new Date(fecha).getMonth()+1);
+        date += '/'+new Date(fecha).getFullYear();
+        return date;
+    }
+
+    $scope.Detalles = function(id){
+        $scope.Detalle = identificar(id, $scope.Facturas);
+
+        if($scope.Detalle != null){
+            $('#modalFacturas').modal('open');
+        }
+    }
+
+    $scope.abrirModal = function(id){
+        var obj = identificar(id, $scope.Facturas);
+        swal({
+            title: "Confirmar Eliminación",
+            text: "¿Esta seguro de borrar la factura?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Si, Borrar!",
+            cancelButtonText: "No, Cancelar!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+        function(isConfirm){
+            if (isConfirm) {
+                Borrar(obj);
+            } else {
+                swal("Cancelado", "La factura no se borrará", "error");
+            }
+        });
+    }
+
+    $scope.Imprimir = function(ele){
+        $scope.Orden = {
+            orden_venta_consecutivo: ele.consecutivo,
+            cliente: ele.cliente,
+            productos: ele.productos
+        }
+        $scope.remision = ele.remision;
+        $scope.ordenCompra = ele.ordenCompra;
+        $scope.subtotal = ele.subtotal;
+        $scope.iva = ele.iva;
+        $scope.valorIva = ele.valorIva;
+        $scope.total = ele.total;
+        if(!$scope.remision){
+            $('#remision').html('');
+        }
+        if(!$scope.ordenCompra){
+            $('#ordenCompra').html('');
+        }
+
+        var w = window.open();
+        var d = w.document.open();
+        var eleToPrint = $('#container')[0];
+        d.append(eleToPrint);
+
+        webServer.getResource('facturas', {} , 'get')
+        .then(function(data){
+            w.print();
+            w.close();
+        }, function(data){
+            w.print();
+            w.close();
+        });
+    }
+
+    function Borrar(ele){
+        preloader.estado = true;
+        webServer.getResource('facturas/'+ele._id, {}, 'put')
+        .then(function(data){
+            swal('Ok', data.data.message, 'success');
+            preloader.estado = false;
+            $scope.Facturas.splice(ele.index, 1);
+        }, function(data){
+            swal('Error', data.data.message, 'error');
+            preloader.estado = false;
+        })
+    }
+
+    function identificar(id, arreglo){
+        var obj = null;
+        arreglo.forEach(function(ele, index){
+            if(ele._id == id){
+                obj = ele;
+                obj.index = index;
+            }
+        });
+        return obj;
+    }
+
 
 });
