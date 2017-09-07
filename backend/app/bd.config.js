5077122b12fc232fe9b79e70fa987f43a315165c
jhoan sebastian arias pulido <jsarias98@misena.edu.co>
2017-09-07 08:56:40 -0500

    build errores de cantidades

--- backend/app/bd.config.js
+++ backend/app/bd.config.js
@@ -1,8 +1,8 @@
-'use strict';
-module.exports  = {
-	user : 'inventory',
-	pass : '900462957-1',
-	host : 'ds113630.mlab.com',
-	port : '13630',
-	data : 'heroku_r0gtxwg1'
-};
+'use strict'
+module.exports = {
+  user: 'inventory',
+  pass: '900462957-1',
+  host: 'ds113630.mlab.com',
+  port: '13630',
+  data: 'heroku_r0gtxwg1'
+}
